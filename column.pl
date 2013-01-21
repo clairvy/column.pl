e777100f8317c3291b0688f8b3d3d36978c0e7bf
nagaya <nagaya@a-tm.co.jp>
2013-01-21 16:32:04 +0900

    use encode for multibyte chars except number with circle

--- column.pl
+++ column.pl
@@ -1,14 +1,17 @@
 #!/usr/bin/env perl
-# -*- mode: perl; -*-
+# -*- mode: perl; code: utf-8; -*-
 
 use 5.008;
 use strict;
 use warnings;
 
 package main;
+use Encode ();
 use File::Basename ();
 use Getopt::Long ();
 use Text::ParseWords ();
+use open ':encoding(UTF-8)';
+use open ':std';
 
 our $VERSION = 0.001;
 our ($Revision) = (q$Revision: 1608 $ =~ m/(\d+)/);
@@ -105,6 +108,27 @@ sub expand_num
     }
     return @num;
 }
+sub zenhan_length
+{
+    my ($str) = @_;
+    my $len = length($str);
+    my $zen = join('', $str =~ m/[^\p{InBasicLatin}\p{InNumberWithCircle}\x{FF64}-\x{FF9F}]+/g);
+    my $zen_len = length($zen);
+    if ($debug_flag) {
+        print "$len $zen_len '", $str, "' '", $zen, "'\n";
+    }
+    return [$len + $zen_len, $zen_len]; # $len - $zen_len + $zen_len * 2 = $len + $zen_len
+}
+# number with circle
+sub InNumberWithCircle
+{
+    return <<"END";
+2460\t2473
+3251\t325F
+32B1\t32BF
+2780\t2789
+END
+}
 sub main
 {
     my $help_flag = 0;
@@ -161,7 +185,7 @@ sub main
         my @token = Text::ParseWords::quotewords($delm, $keep, $line);
         map {s/^\s+//;s/\s+$//;} @token if ($trim_flag);
         @token = map {defined($_) ? $_ : ''} @token;
-        my @len_buf = map {length($_)} @token;
+        my @len_buf = map {zenhan_length($_)->[0]} @token;
         foreach my $i (0 .. scalar(@token) - 1) {
             next unless (not defined($len[$i]) or ($len[$i] < $len_buf[$i]));
             $len[$i] = $len_buf[$i];
@@ -187,10 +211,14 @@ sub main
             if (exists($right->{$i})) {
                 $align = '';
             }
+            my $zen_len = zenhan_length($token[$i])->[1];
+            print "MAX:$len ZEN:$zen_len\n" if $debug_flag;
+            $len -= $zen_len;
             if ($i == $last_index and $align eq '-') {
                 $len = 0;
             }
             my $f = "%" . $align . $len . "s";
+            print "$f\n" if $debug_flag;
             push(@pbuf, sprintf($f, $token[$i]));
         }
         print(join($print_delm, @pbuf), "\n");
